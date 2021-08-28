d216ebe94c154b1582cb4b34a5b060b32f12d8d2
leida <zhang20084@126.com>
2021-08-28 14:01:14 +0800

    fix:路由增加base参数

--- api/account.js
+++ api/account.js
@@ -6,14 +6,3 @@
 export const getUser = (context) => {
   return context.$axios.get(`${process.env.baseUrl}/api/account`)
 }
-
-/**
- * 修改密码
- *
- * @see https://www.easyapi.com
- */
-export const changePassword = (data, context) => {
-  return context.$axios.post(`${process.env.baseUrl}/api/account/change-password`, {
-    ...data
-  })
-}
